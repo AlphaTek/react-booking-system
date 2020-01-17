3b6506d5ec16eebc3091bf4232b142e83d15b613
awasif647 <chose2bupontruth@gmail.com>
2020-01-17 01:10:18 -0500

    react

---
+++ src/main/resources/static/precache-manifest.edef90a1ab585b5e77dad0339535986b.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb5c866c92ce0478985fd4902013e71a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "88f5be810cc26df37022",
+    "url": "/static/css/2.ff9ea1f9.chunk.css"
+  },
+  {
+    "revision": "e503c5ce53671bc68493",
+    "url": "/static/css/main.0fef8d25.chunk.css"
+  },
+  {
+    "revision": "88f5be810cc26df37022",
+    "url": "/static/js/2.2b656f4d.chunk.js"
+  },
+  {
+    "revision": "e503c5ce53671bc68493",
+    "url": "/static/js/main.b7b93384.chunk.js"
+  },
+  {
+    "revision": "c9cb648424b20eaa98f2",
+    "url": "/static/js/runtime~main.b7c43ba8.js"
+  },
+  {
+    "revision": "fbcf836b1bf577ca2d9b937e7f369359",
+    "url": "/static/media/AP-book.fbcf836b.png"
+  },
+  {
+    "revision": "ecaf4209b30406e53c1587c7946bf703",
+    "url": "/static/media/calendar.ecaf4209.png"
+  },
+  {
+    "revision": "e2ca9eb83c2bcc85321dab354de8167e",
+    "url": "/static/media/contact-us-programs.e2ca9eb8.png"
+  },
+  {
+    "revision": "96b33a75ba3a9136e22e5c07327254df",
+    "url": "/static/media/entertainment.96b33a75.jpg"
+  },
+  {
+    "revision": "921edb43e3d19cd1e4e1872bdb8c7344",
+    "url": "/static/media/fitness.921edb43.jpg"
+  },
+  {
+    "revision": "7fb0bf591bf8fedfa2d278eb4d97a3ef",
+    "url": "/static/media/gymnastics.7fb0bf59.jpg"
+  },
+  {
+    "revision": "b5759d8e6d6077ac3ccd391b77369986",
+    "url": "/static/media/kidsTest.b5759d8e.jpg"
+  },
+  {
+    "revision": "3f52c2a864bdbc8cc45048a5545dfc68",
+    "url": "/static/media/other.3f52c2a8.jpg"
+  },
+  {
+    "revision": "46bb2dcde7ec8d925e1927db2ded7cc2",
+    "url": "/static/media/parkour.46bb2dcd.jpg"
+  },
+  {
+    "revision": "f5894a98d1c3ec0ba9829a938a8e7c1e",
+    "url": "/static/media/parkourHome.f5894a98.jpg"
+  },
+  {
+    "revision": "a04ef3c33401d06c694b3b4585066b04",
+    "url": "/static/media/parkourHome2.a04ef3c3.jpg"
+  },
+  {
+    "revision": "a5d7d09ab267475bf9a446f1591c17d5",
+    "url": "/static/media/parkourLogin2.a5d7d09a.jpg"
+  },
+  {
+    "revision": "1ba956250b535358a0610324f021c3a6",
+    "url": "/static/media/parkourRegister.1ba95625.jpg"
+  },
+  {
+    "revision": "354f2d096db5a4d07d479809641d534d",
+    "url": "/static/media/parkourhomepage.354f2d09.png"
+  },
+  {
+    "revision": "8fc5351bd2e6ce35466be93924bc3571",
+    "url": "/static/media/spykidrope.8fc5351b.jpg"
+  }
+]);
